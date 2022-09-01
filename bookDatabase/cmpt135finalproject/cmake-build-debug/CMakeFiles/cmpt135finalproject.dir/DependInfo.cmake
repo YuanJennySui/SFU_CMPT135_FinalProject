11df211137ab90c9a8c2a424464548c9c241e375
Yuan Sui <44080247+YuanJennySui@users.noreply.github.com>
2022-08-31 22:19:43 -0400

    Add files via upload

---
+++ bookDatabase/cmpt135finalproject/cmake-build-debug/CMakeFiles/cmpt135finalproject.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jenny/CLionProjects/cmpt135finalproject/Book.cpp" "/home/jenny/CLionProjects/cmpt135finalproject/cmake-build-debug/CMakeFiles/cmpt135finalproject.dir/Book.cpp.o"
+  "/home/jenny/CLionProjects/cmpt135finalproject/Database.cpp" "/home/jenny/CLionProjects/cmpt135finalproject/cmake-build-debug/CMakeFiles/cmpt135finalproject.dir/Database.cpp.o"
+  "/home/jenny/CLionProjects/cmpt135finalproject/Menu.cpp" "/home/jenny/CLionProjects/cmpt135finalproject/cmake-build-debug/CMakeFiles/cmpt135finalproject.dir/Menu.cpp.o"
+  "/home/jenny/CLionProjects/cmpt135finalproject/main.cpp" "/home/jenny/CLionProjects/cmpt135finalproject/cmake-build-debug/CMakeFiles/cmpt135finalproject.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ bookDatabase/cmpt135finalproject/cmake-build-debug/CMakeFiles/cmpt135finalproject.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmpt135finalproject.dir/main.cpp.o"
+  "CMakeFiles/cmpt135finalproject.dir/Database.cpp.o"
+  "CMakeFiles/cmpt135finalproject.dir/Book.cpp.o"
+  "CMakeFiles/cmpt135finalproject.dir/Menu.cpp.o"
+  "cmpt135finalproject.pdb"
+  "cmpt135finalproject"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmpt135finalproject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
